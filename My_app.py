54a55ac1bb133609aae05b3678832426d5b25f33
RomanSukhai <romansukhai@gmail.com>
2023-11-02 10:37:04 +0200

    new udate

--- My_app.py
+++ My_app.py
@@ -1,12 +1,21 @@
-from flask import Flask
+from flask import Flask, jsonify
+from flasgger import Swagger
 
 app = Flask(__name__)
+swagger = Swagger(app)
 
 VARIANT_NUMBER = 20
 
 @app.route(f"/api/v1/hello-world-{VARIANT_NUMBER}")
 def hello_world():
-    return f"Hello World {VARIANT_NUMBER}", 200
+    """
+    Hello World endpoint
+    ---
+    responses:
+      200:
+        description: A simple hello world
+    """
+    return jsonify(message=f"Hello World {VARIANT_NUMBER}"), 200
 
 if __name__ == "__main__":
     app.run()
